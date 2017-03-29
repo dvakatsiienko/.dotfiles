bbcda1f991209783df1616b304276b86d4c9ba01
d.vakatsiienko <imagnum.satellite@gmail.com>
2017-03-29 15:16:59 +0300

    add shell script install scenario :tropical_fish:

--- initialize.sh
+++ initialize.sh
@@ -1,15 +1,47 @@
 # Pre-requisite - system setup ssh agent with keys
 
+# mkdir ~/.ssh
+# ssh-keygen -t rsa -b 4096 -C "imagnum.satellite@gmail.com"
+# eval "$(ssh-agent -s)"
+# ssh-add -K ~/.ssh/github
+# pbcopy < ~/.ssh/github.pub
+# git clone git@github.com:mike-midnight/.dotfiles.git
+
+# TODO
+# up vote
+# 93
+# down vote
+# accepted
+# You can also change the preference keys directly:
+# 
+# defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+# defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+# The changes aren't applied until you log out and back in. KeyRepeat can't be set between 2 (30 ms) and 1 (15 ms) though.
+# 
+# I also use KeyRemap4MacBook. I've set the repeat rates to 40 ms and the initial repeat rates to 150 ms.
+
+# Command line tools
+xcode-select --install
+
+# omzsh install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# switch to zsh
+# chsh -s $(which zsh)
+# exec zsh
+
+# remove .bash meta info
+rm ~/.bash_history
+rm -rf ~/.bash_sessions
+
+# Homebrew permissions fix
+sudo chown -R "$USER":admin /usr/local
+
 # Brew
-ruby -e "$(curl -fsSL https://
-raw.githubusercontent.com/Homebrew/install/master/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Vim Plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# brew-cask install
-brew install brew-cask
+# Vim Plug install
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # ctags for vim
 brew install ctags
@@ -17,11 +49,16 @@ brew install ctags
 # hub - command-line wrapper for git that makes you better at GitHub
 brew install hub
 
-# brew formulas
-brew cask install chrome
-brew cask install node
-brew cask install yarn
-brew cask install the_silver_searcher
+# Classic formulas
+brew install node
+brew install yarn
+brew install the_silver_searcher
+
+# Node permissions fix
+sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+
+# brew cask formulas
+brew cask install google-chrome
 brew cask install google-drive 
 brew cask install skyfonts
 brew cask install skype
@@ -29,30 +66,12 @@ brew cask install atom
 brew cask install webstorm
 brew cask install alfred
 
-# Node permissions
-sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+# Install powerline fonts
+git clone git@github.com:powerline/fonts.git ~/fonts && sh ~/fonts/install.sh && rm -rf ~/fonts
 
-# Command line tools 
-xcode-select --install
+# Install nerd fonts
+git clone git@github.com:ryanoasis/nerd-fonts.git ~/nerd-fonts && sh ~/nerd-fonts/install.sh && rm -rf ~/nerd-fonts
 
-# zsh install
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# switch to zsh
-chsh -s $(which zsh)
-exec zsh
-
-# remove .bash meta info
-cd ~
-rm .bash_history
-rm -rf .bash_sessions
-
-# install powerline fonts
-git clone https://github.com/powerline/fonts.git && cd fonts && sh install.sh && cd .. && rm -rf fonts
-
-# install nerd fonts
-git clone git@github.com:ryanoasis/nerd-fonts.git && cd nerd-fonts && sh install.sh && cd .. && rm -rf nerd-fonts
-
-# turn off FaceTime sounds on mac
+# Turn off FaceTime sounds on mac
 sudo defaults write ~/Library/Containers/com.apple.tonelibraryd/Data/Library/Preferences/com.apple.ToneLibrary.plist ringtone "system:"
 
